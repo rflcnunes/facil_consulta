0841e26eac1672ce709547823e9629a15df36949
Rafael <58157026+rflcnunes@users.noreply.github.com>
2022-05-26 03:08:09 -0300

    Created FormComponent

---
+++ src/components/HeaderTextComponent.vue
@@ -0,0 +1,22 @@
+<template>
+<div id="header-text-component">
+  <h2 id="principal-text">{{ title }}</h2>
+</div>
+</template>
+
+<script>
+export default {
+  name: "HeaderTextComponent",
+  props: ['title']
+}
+</script>
+
+<style scoped>
+#principal-text {
+  font-family: 'Comfortaa', cursive;
+  margin-top: 10px;
+  margin-bottom: 25px;
+  color: #483698;
+}
+
+</style>
