51d9f5317244eb43d6bf0faa0e5fad144ef1bd9a
Rafael <rafaelcnunes@icloud.com>
2022-05-26 03:11:38 -0300

    Created SubtitleComponent

---
+++ src/components/SubtitleTextComponent.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="subtitle-text-component">
+    <h5 id="secondary-text">{{ title }}</h5>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SubtitleTextComponent",
+  props: ['title']
+}
+</script>
+
+<style scoped>
+#subtitle-text-component {
+  margin-bottom: 25px;
+}
+</style>
