80413e6240a87788f897ed57c03cb6d9a06f0d01
Rafael <rafaelcnunes@icloud.com>
2022-05-26 03:09:55 -0300

    Created InputCpfComponent

---
+++ src/components/InputCpfComponent.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="input-component">
+    <label id="label" for="input">{{ title }}</label>
+    <the-mask id="input" :mask="['###.###.###-##']" :placeholder="placeholder"/>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: "InputCpfComponent",
+  props: ['title', 'placeholder'],
+}
+</script>
+
+<style scoped>
+#input-component {
+  display: flex;
+  flex-direction: column;
+  max-width: 65%;
+  min-width: 65%;
+  font-family: 'Open Sans', sans-serif;
+  margin-top: 5%;
+  margin-bottom: 5%;
+}
+
+#label {
+  font-size: smaller;
+  margin-bottom: 5px;
+}
+
+#input {
+  border: 1px solid #483698;
+  border-radius: 5px;
+  font-size: smaller;
+  padding: 3px 0 3px 13px;
+}
+
+input:focus {
+  outline: none !important;
+}
+</style>
