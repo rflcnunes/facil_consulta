960187e718fe10fa9456bc9a87762d5c40dd26d9
Rafael <rafaelcnunes@icloud.com>
2022-05-26 03:09:09 -0300

    Created InputComponent

---
+++ src/components/InputComponent.vue
@@ -0,0 +1,42 @@
+<template>
+<div id="input-component">
+  <label id="label" for="input">{{ title }}</label>
+  <input id="input" type="text" :placeholder="placeholder">
+</div>
+</template>
+
+<script>
+export default {
+  name: "InputComponent",
+  props: ['title', 'placeholder']
+}
+</script>
+
+<style scoped>
+#input-component {
+  display: flex;
+  flex-direction: column;
+  max-width: 100%;
+  min-width: 100%;
+  font-family: 'Open Sans', sans-serif;
+  margin-top: 5%;
+  margin-bottom: 5%;
+}
+
+#label {
+  font-size: smaller;
+  margin-bottom: 5px;
+}
+
+#input {
+  border: 1px solid #483698;
+  border-radius: 5px;
+  font-size: smaller;
+  padding: 3px 0 3px 13px;
+}
+
+input:focus {
+  outline: none !important;
+}
+
+</style>
